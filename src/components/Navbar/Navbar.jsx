a9b6444b58491eb7e274a380ab997e58f57df1fa
Mohamed22Yousri <mohamedyousrisadeq2003@gmail.com>
2026-07-15 14:42:27 +0300

    Update navbar mobile search

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,13 +8,19 @@ import nav_dropdown from "../Assets/nav-dropdown.png";
 
 function Navbar() {
   const cartRef = useRef();
+
   const [menu, setMenu] = useState("shop");
+  const [search, setSearch] = useState("");
+
   const { getTotalCartItems } = useContext(ShopContext);
+
   const menuRef = useRef();
+
   const dropdown_toggle = (e) => {
     menuRef.current.classList.toggle("nav-menu-visible");
     e.target.classList.toggle("open");
   };
+
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -22,32 +28,37 @@ function Navbar() {
           <img src={logo} alt="" />
         </Link>
       </div>
+
       <img
         className="nav-dropdown open"
         onClick={dropdown_toggle}
         src={nav_dropdown}
         alt=""
       />
+
       <ul ref={menuRef} className="nav-menu nav-menu-visible">
         <li onClick={() => setMenu("vegetables")}>
-          <Link to="/vegetables">خضروات</Link>{" "}
-          {menu === "vegetables" ? <hr /> : <></>}
+          <Link to="/vegetables">الخضروات</Link>
+          {menu === "vegetables" && <hr />}
         </li>
 
         <li onClick={() => setMenu("fruits")}>
-          <Link to="/fruits">فواكهه</Link> {menu === "fruits" ? <hr /> : <></>}
+          <Link to="/fruits">الفواكهه</Link>
+          {menu === "fruits" && <hr />}
         </li>
 
         <li onClick={() => setMenu("shop")}>
-          {" "}
           <Link to="/">الرئيسية</Link>
-          {menu === "shop" ? <hr /> : <></>}
+          {menu === "shop" && <hr />}
         </li>
       </ul>
+    
+
       <div className="nav-logoing-cart">
-      <Link to="/cart">
-  <img ref={cartRef} src={cart} alt="" id="cart-icon" />
-</Link>
+        <Link to="/cart">
+          <img ref={cartRef} src={cart} alt="" id="cart-icon" />
+        </Link>
+
         <div className="nav-count">{getTotalCartItems()}</div>
       </div>
     </div>
